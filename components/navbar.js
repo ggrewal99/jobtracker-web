a81cc797e5bcf788a98c85326d17a2467ea38aa2
ggrewal99 <guruvindergrewal98@gmail.com>
2025-06-15 13:29:31 +1000

    bugfix: Styling for active link in mobile navbar

--- components/navbar.js
+++ components/navbar.js
@@ -150,28 +150,20 @@ export default function Navbar() {
 
 			<DisclosurePanel className='md:hidden'>
 				<div className='space-y-1 pt-2 pb-3'>
-					{/* Current: "bg-indigo-50 border-blue-400 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-					<DisclosureButton
-						as='a'
-						href='/'
-						className='block border-l-4 border-blue-400 bg-blue-50 py-2 pr-4 pl-3 text-base font-medium text-blue-600 sm:pr-6 sm:pl-5'
-					>
-						Dashboard
-					</DisclosureButton>
-					<DisclosureButton
-						as='a'
-						href='/myJobs'
-						className='block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pr-6 sm:pl-5'
-					>
-						Jobs
-					</DisclosureButton>
-					<DisclosureButton
-						as='a'
-						href='/myTasks'
-						className='block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pr-6 sm:pl-5'
-					>
-						Tasks
-					</DisclosureButton>
+					{links.map((link) => (
+						<DisclosureButton
+							key={link.name}
+							as='a'
+							href={link.href}
+							className={`block border-l-4 py-2 pr-4 pl-3 text-base font-medium sm:pr-6 sm:pl-5 ${
+								pathname === link.href
+									? 'border-blue-400 bg-blue-50 text-blue-600'
+									: 'border-transparent text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700'
+							}`}
+						>
+							{link.name}
+						</DisclosureButton>
+					))}
 				</div>
 				<div className='border-t border-gray-200 pt-4 pb-3'>
 					<div className='flex items-center px-4 sm:px-6'>
@@ -192,24 +184,22 @@ export default function Navbar() {
 							</svg>
 						</div>
 						<div className='ml-3'>
-							{user ? (
+							{user && (
 								<>
 									<div className='text-base font-medium text-gray-800'>
-										{user?.firstName} {user?.lastName}
+										{user.firstName} {user.lastName}
 									</div>
 									<div className='text-sm font-medium text-gray-500'>
-										{user?.email}
+										{user.email}
 									</div>
 								</>
-							) : (
-								''
 							)}
 						</div>
 					</div>
 					<div className='mt-3 space-y-1'>
 						<DisclosureButton
 							as='a'
-							href='#'
+							href='/profile'
 							className='block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6'
 						>
 							Your Profile
